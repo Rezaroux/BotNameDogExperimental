7d9d88c01d928498336d4ba5b83bbf5d0a6d9583
spesh-al <60618980+spesh-al@users.noreply.github.com>
2021-09-17 16:50:28 -0500

    fart
    why

--- index.ts
+++ index.ts
@@ -3,22 +3,35 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
-    ]
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
 client.on('ready', () => {
     console.log("BotNDE is online")
+
+    const guildId = '726196852646608897'
+    const guild = client.guilds.cache.get(guildId)
+    let commands
+
+    if (guild) {
+        commands = guild.commands
+    } else {
+        commands = client.application?.commands
+    }
+
+    commands?.create({
+        name: 'ping',
+        description: 'pongs'
+    })
 })
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'fart') {
-        message.reply({
-            content: 'nugget',
-        })
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return
     }
+
+    const { commandName, options } = interaction
+
 })
 
 client.login(process.env.TOKEN)
