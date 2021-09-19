17d9d51cdf4781cc004c39c6bc2c0b2df6a6443b
spesh-al <60618980+spesh-al@users.noreply.github.com>
2021-09-19 17:48:56 -0500

    slash command support

--- index.ts
+++ index.ts
@@ -1,37 +1,27 @@
 import DiscordJS, { Intents } from 'discord.js'
+import WOKCommands from 'wokcommands'
+import path from 'path'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS,]
 })
 
 client.on('ready', () => {
     console.log("BotNDE is online")
+    
+    new WOKCommands(client, {
+        commandsDir: path.join(__dirname, 'commands'),
+        typeScript: true,
+        testServers: ['726196852646608897'],
+        
+        disabledDefaultCommands: [
 
-    const guildId = '726196852646608897'
-    const guild = client.guilds.cache.get(guildId)
-    let commands
-
-    if (guild) {
-        commands = guild.commands
-    } else {
-        commands = client.application?.commands
-    }
-
-    commands?.create({
-        name: 'ping',
-        description: 'pongs'
+        ]
     })
-})
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-        return
-    }
-
-    const { commandName, options } = interaction
-
+    .setDefaultPrefix(';')
+    .setColor(0xEB4444)
 })
 
 client.login(process.env.TOKEN)
